341020902c2f141efbd9fa17955b6e4b31c18253
Kaai Suzuki <suzydp@me.com>
2018-12-11 13:39:53 -0800

    Apply header

---
+++ src/Album.jsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+// import from MUI
+import AppBar from '@material-ui/core/AppBar';
+import CameraIcon from '@material-ui/icons/PhotoCamera';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import { Toolbar } from '@material-ui/core';
+
+
+const styles = theme => ({
+    appBar: {
+      position: 'relative',
+    },
+    icon: {
+      marginRight: theme.spacing.unit * 2, //margin-right:00
+    },
+    heroUnit: {
+      backgroundColor: theme.palette.background.paper, //background-color:#000000;
+    },
+    // heroContent:{
+    //   maxWidth:600, //max-width:600;
+    //   margin:`0 auto`, //margin:0 auto;
+    //   padding:`${theme.spacing.unit * 8}px 0 ${theme.spacing.unit *6}px` //padding:00px 0 00px
+    // },
+    // heroButtons:{
+    //   marginTop: theme.spacing.unit * 4,
+    // },
+    // layout:{
+    //   width:'auto',
+    //   marginLeft:theme.spacing.unit * 3,
+    //   marginRight:theme.spacing.unit * 3,
+    //   [theme.breakpoints.up(1100 + theme.spacing.unit * 3 * 2)]:{
+    //     width:1100,
+    //     marginLeft:'auto',
+    //     marginRight: 'auto',
+    //   },
+    // },
+    // cardGrid:{
+    //   padding: `${theme.spacing.unit * 8}px 0`,
+    // },
+    // card:{
+    //   height:'100%',
+    //   display:'flex',
+    //   flexDirection: 'column',
+    // },
+    // cardMedia:{
+    //   paddingTop: '56.25%', //16:9
+    // },
+    // cardContent:{
+    //   flexGrow:1,
+    // },
+    // footer:{
+    //   backgroundColor: theme.palette.background.paper,
+    //   padding: theme.spacing.unit * 6,
+    // },
+});
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const Album = (props) => {
+  const { classes } = props;
+
+  return(
+    <React.Fragment>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
+          <CameraIcon className={classes.icon} />
+          <Typography variant="h6" color="inherit" nowrap>Album Layout</Typography>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  )
+}
+
+Album.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Album);
--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import Album from './Album.jsx';
 import Button from '@material-ui/core/Button';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <Button variant="contained" color="primary">Hello World</Button>  
-    );
+      <React.Fragment>
+        <Album/>
+        <Button variant="contained" color="primary">Hello World</Button>
+      </React.Fragment>
+      
+    )
   }
 }
 
